dde5d064a48cb14c1bd1451dfd719a27c8ac720f
deadlysinalex <114429974+deadlysinalex@users.noreply.github.com>
2022-09-30 13:53:54 -0400

    Add files via upload

---
+++ Java Files/Scanner_1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author Alexander
+ */
+import java.util.Scanner;
+public class Scanner_1 {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter Your Name");
+
+    String name = input.nextLine();
+    System.out.println("My name is" + name);
+        
+        
+    }
+}
